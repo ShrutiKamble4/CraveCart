d69e256ae33371f48c6d7d964a7422bc12fe48cc
shruti <kshruti23cs@student.mes.ac.in>
2026-02-15 20:14:47 +0530

    Fixed Railway MYSQL_URL parsing

--- src/java/com/cravecart/db/DBConnection.java
+++ src/java/com/cravecart/db/DBConnection.java
@@ -6,31 +6,38 @@ import java.net.URI;
 public class DBConnection {
 
     public static Connection getConnection() {
-        try {
+    try {
 
-            String mysqlUrl = System.getenv("MYSQL_URL");
+        String rawUrl = System.getenv("MYSQL_URL");
 
-            if (mysqlUrl == null) {
-                System.out.println("MYSQL_URL is NULL");
-                return null;
-            }
-
-            URI uri = new URI(mysqlUrl);
-
-            String userInfo = uri.getUserInfo();
-            String username = userInfo.split(":")[0];
-            String password = userInfo.split(":")[1];
-
-            String jdbcUrl = "jdbc:mysql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath() +
-                    "?useSSL=false&allowPublicKeyRetrieval=true";
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            return DriverManager.getConnection(jdbcUrl, username, password);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (rawUrl == null) {
+            System.out.println("MYSQL_URL is NULL");
             return null;
         }
+
+        // Remove mysql://
+        rawUrl = rawUrl.replace("mysql://", "");
+
+        String[] parts = rawUrl.split("@");
+        String[] userPass = parts[0].split(":");
+        String[] hostDb = parts[1].split("/");
+
+        String username = userPass[0];
+        String password = userPass[1];
+        String hostPort = hostDb[0];
+        String database = hostDb[1];
+
+        String jdbcUrl = "jdbc:mysql://" + hostPort + "/" + database +
+                "?useSSL=false&allowPublicKeyRetrieval=true";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(jdbcUrl, username, password);
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        return null;
     }
+}
+
 }
