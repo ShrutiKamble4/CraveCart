71e27cc5e4308c85bc17d99b613dbae378881446
shruti <kshruti23cs@student.mes.ac.in>
2026-02-16 08:05:40 +0530

    updated

--- src/java/com/cravecart/db/DBConnection.java
+++ src/java/com/cravecart/db/DBConnection.java
@@ -6,38 +6,35 @@ import java.net.URI;
 public class DBConnection {
 
     public static Connection getConnection() {
-    try {
+        try {
 
-        String rawUrl = System.getenv("MYSQL_URL");
+            String rawUrl = System.getenv("MYSQL_URL");
 
-        if (rawUrl == null) {
-            System.out.println("MYSQL_URL is NULL");
+            if (rawUrl == null) {
+                System.out.println("MYSQL_URL is NULL");
+                return null;
+            }
+
+            URI uri = new URI(rawUrl);
+
+            String username = uri.getUserInfo().split(":")[0];
+            String password = uri.getUserInfo().split(":")[1];
+
+            String host = uri.getHost();
+            int port = uri.getPort();
+            String database = uri.getPath().replaceFirst("/", "");
+
+            String jdbcUrl = "jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "?useSSL=false&allowPublicKeyRetrieval=true";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection(jdbcUrl, username, password);
+
+        } catch (Exception e) {
+            System.out.println("Database Connection Failed!");
+            e.printStackTrace();
             return null;
         }
-
-        // Remove mysql://
-        rawUrl = rawUrl.replace("mysql://", "");
-
-        String[] parts = rawUrl.split("@");
-        String[] userPass = parts[0].split(":");
-        String[] hostDb = parts[1].split("/");
-
-        String username = userPass[0];
-        String password = userPass[1];
-        String hostPort = hostDb[0];
-        String database = hostDb[1];
-
-        String jdbcUrl = "jdbc:mysql://" + hostPort + "/" + database +
-                "?useSSL=false&allowPublicKeyRetrieval=true";
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        return DriverManager.getConnection(jdbcUrl, username, password);
-
-    } catch (Exception e) {
-        e.printStackTrace();
-        return null;
     }
-}
-
 }
